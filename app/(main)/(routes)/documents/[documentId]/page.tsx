b14ae88300cddc22f28aa48d64f523c5c61ba74c
oliversolomon <oliversolomonwainaina@gmail.com>
2023-10-19 00:37:45 +0300

    feat: Add document page Icon component and toolbar component
    This commit adds the `DocumentIdPage` component and the `Toolbar` component to the codebase. The `DocumentIdPage` component fetches a document by its ID using the `useQuery` hook, handles loading and not found states, and renders the document with a toolbar. The `Toolbar` component displays an icon, a title, and provides options to edit the title, add/edit an icon, and add a cover image.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,42 @@
-export default function DocumentIdPage() {
+"use client"
+
+import {useQuery} from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { Toolbar } from "@/components/toolbar"
+
+interface DocumentIdPageProps {
+    params: {
+        documentId: Id<"documents">
+    }
+}
+
+export default function DocumentIdPage({params}:DocumentIdPageProps) {
+    const document = useQuery(api.documents.getById, {documentId: params.documentId})
+
+    if (document === undefined) {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    if (document === null) {
+        return(
+            <div>
+                Not Found
+            </div>
+        )
+    }
+
     return(
-        <div className="flex items-center justify-center border-2 flex-1 min-h-full text-red-300 font-bold text-lg">
-            Here is your created document
+        <div className="pb-40">
+            <div className="h-[35vh]"/>
+            {/* <div className="md:max-w-3xl lg:max-w-4xl mx-auto"> */}
+            <div className="md:max-w-3xl lg:max-w-4xl">
+                <Toolbar initialData={document}/>
+            </div>
         </div>
     )
 }
