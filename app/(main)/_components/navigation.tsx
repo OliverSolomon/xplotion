14b04a390174440813eed7c9bb2a3d078a1e3470
oliversolomon <oliversolomonwainaina@gmail.com>
2023-10-18 21:59:22 +0300

    Add Navbar, Title component and getById and update methods
    - Added a new file `navbar.tsx` which contains a `Navbar` component responsible for rendering the navigation bar.
    - Added a new file `title.tsx` which contains a `Title` component responsible for rendering the title of a document.
    - Added a new query method `getById` in `documents.ts` which retrieves a document given its ID.
    - Added a new mutation method `update` in `documents.ts` which updates the information of a document.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react"
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
@@ -15,8 +15,10 @@ import {Popover, PopoverTrigger, PopoverContent} from "@/components/ui/popover"
 import TrashBox from "./trash-box";
 import {useSearch} from "@/hooks/use-search"
 import { useSettings } from "@/hooks/use-settings";
+import {Navbar} from "./navbar";
 
 export function Navigation() {
+    const params = useParams()
     const search = useSearch()
     const settings= useSettings()
     const pathname = usePathname()
@@ -149,13 +151,15 @@ export function Navigation() {
         </aside>
         <div ref={navbarRef} className={cn("absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]", 
         isResetting && "transition-all ease-in-out duration-300",
-        isMobile && "left-0 w-full"
-
-)}>
-            <nav  className="bg-transparent px-3 py-2 w-full">
+        isMobile && "left-0 w-full")}>
+             {!!params.documentId ? (
+                <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+             ) : (
+                <nav  className="bg-transparent px-3 py-2 w-full">
                 {isCollapsed && <MenuIcon 
              onClick = {resetWidth} role="button" className="h-6 w-6 text-muted-foreground"/>}
-            </nav>
+            </nav>)
+             }
         </div>
         </>
 )}
